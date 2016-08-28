6eac8e338a6e36d9d7096d5fd3bc49cbf6fc0970
Han Chen <hanchen1996@hotmail.com>
2016-08-28 02:33:40 -0400

    supporting toggling loading status

--- src/directives/ept-loading-bar/ept-loading-bar.js
+++ src/directives/ept-loading-bar/ept-loading-bar.js
@@ -1,15 +1,19 @@
-import angular from "angular";
+import angular from 'angular';
 require('./ept-loading-bar.less');
-export default angular.module("ept.common.directives.eptLoadingBar",[]).directive('eptLoadingBar', [function () {
+export default angular.module('ept.common.directives.eptLoadingBar',[]).directive('eptLoadingBar', [function () {
 	return {
 		priority: 0,
 		template: require('./ept-loading-bar.html'),
 		transclude: true,
 		restrict: 'E',
 		scope: {
-			cssClass:"=class" //optional css class
+			cssClass:'=class', //optional css class
+			displayType:'=',
+			isLoading:'='
 		},
-		controller: function($scope, $element, $attrs) {},
+		controller: function($scope, $element, $attrs) {
+			$element.parent().css({'position':'relative'});
+		},
 		link: function postLink(scope, iElement, iAttrs) {
 
 		}
